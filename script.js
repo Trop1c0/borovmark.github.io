e2c7ea514d4782393077009b8212a52969b1d4f5
borov <m1r4cl311@yandex.ru>
2025-09-17 22:13:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+
+// Анимация появления секций при скролле
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.classList.add('visible');
+}
+});
+}, { threshold: 0.2 });
+
+
+document.querySelectorAll('.intro, .card').forEach(el => observer.observe(el));
